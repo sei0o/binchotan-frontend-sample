408af5aadf4f8685af646c13e632f58e6c47363c
Seioo Inoue <sei0o@live.jp>
2022-08-16 22:15:02 +0900

    Implement status subcommand

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use clap::{Parser, Subcommand};
+use serde_json::json;
+use std::{
+    env,
+    io::{Read, Write},
+    os::unix::net::UnixStream,
+};
+
+use anyhow::Result;
+
+const JSONRPC_VERSION: &str = "2.0";
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Calls status method
+    Status,
+
+    /// Calls plain_request method
+    Plain {
+        // TODO
+    },
+
+    /// Calls home_timeline method
+    Home {
+        // TODO
+    },
+}
+
+fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let sock_path = env::var("SOCKET_PATH")?;
+    let mut stream = UnixStream::connect(sock_path)?;
+    let id = uuid::Uuid::new_v4().to_string();
+
+    let args = Args::parse();
+    match args.command {
+        Commands::Status => {
+            let payload = json!({
+                "jsonrpc": JSONRPC_VERSION,
+                "id": id,
+                "method": "v0.status",
+            })
+            .to_string();
+            let mut resp = String::new();
+            stream.write_all(payload.as_bytes())?;
+            stream.read_to_string(&mut resp)?;
+
+            println!("{}", resp);
+        }
+        Commands::Plain {} => todo!(),
+        Commands::Home {} => todo!(),
+    }
+
+    Ok(())
+}
